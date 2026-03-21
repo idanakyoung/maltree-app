cca83f5ad30f4a94bc0a1fe93bf714b26e61a44f
idanakyoung <idanakyoung@gmail.com>
2026-03-21 15:24:25 +0900

    docs: update README with project overview

---
+++ lib/app/router/route_names.dart
@@ -0,0 +1,9 @@
+class RouteNames {
+  static const home = '/';
+  static const exams = '/exams';
+  static const parts = '/parts';
+  static const types = '/types';
+  static const templateDetail = '/template-detail';
+  static const templateCreate = '/template-create';
+  static const templateEdit = '/template-edit';
+}
